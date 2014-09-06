a9fa1549a35ae296f8e17fee9db08ac7682b09bf
Srijay Kasturi <srijay@techfilmer.com>
2014-09-06 13:10:44 -0400

    now that it mostly works, it's time for shiny ui/x :D

--- app/blog.py
+++ app/blog.py
@@ -1,13 +1,21 @@
 from app import app
-from flask import jsonify, request
+from flask import jsonify, request, render_template, redirect
 from pymongo import MongoClient
+from json import loads as json
 
 client = MongoClient()
 
-@app.route('/blog', methods = ['POST'])
-def reader():
-    input_ = request.get_json()
+@app.route('/api/blog', methods = ['POST'])
+def blogger():
+    input_ = request.form.to_dict()
     db = client.findablogforme
-    db.blog.insert(input_)
+    input_["topics"] = input_["topics"].lower().replace(" ", "").split(",")
+    #input_["topics"] = json(str(input_["topics"]).lower().replace("'","\""))
+    db.blogs.insert(input_)
     del input_["_id"]
-    return jsonify(input_)
+    return redirect("/")
+
+@app.route('/blog', methods = ['GET'])
+def submit_blog():
+    
+    return render_template("blog.html")
--- app/read.py
+++ app/read.py
@@ -1,18 +1,31 @@
 from app import app
-from flask import jsonify, request
+from flask import jsonify, request, render_template
 from pymongo import MongoClient
 from random import choice
-
+from json import loads as json
 client = MongoClient()
 
-@app.route('/read', methods = ['GET'])
+@app.route('/api/read', methods = ['GET'])
 def reader():
-    input_ = request.get_json()
+    query = request.args.to_dict()
+    print(query)
     db = client.findablogforme
-    db.read.insert(input_)
-    del input_["_id"]
+    db.read.insert(query)
+    del query["_id"]
+    print(query)
     blogs = []
-    for blog in db.blogs.find(input_):
+    query = json(str(query).lower().replace("'", "\""))
+    print(query)
+    empty_keys = [k for k,v in query.items() if len(v) < 1] 
+    for i in empty_keys:
+        del query[i]
+    for blog in db.blogs.find(query):
         del blog["_id"]
         blogs.append(blog)
+    if len(blogs) == 0:
+        return jsonify([])
     return jsonify(choice(blogs))
+
+@app.route('/')
+def find_blogs():
+   return render_template("index.html") 
